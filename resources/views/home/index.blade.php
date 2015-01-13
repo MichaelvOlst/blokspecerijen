52d2e0c1e764b6508931806140c50c8a69fdc9be
Michael <michaeltje@live.nl>
2015-01-13 12:04:18 +0100

    first commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+
+@section('content')
+	<h1>Hello from index</h1>
+@stop
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<link href='http://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+
+		{!!HTML::style('css/app.css')!!}
+
+	</head>
+	<body>
+
+	    <header>
+	        <h1>{!! link_to_route('home', 'Blokspecerijen') !!}</h1>
+
+	        <nav>
+	           <ul>
+	               <li><a href="#">Welkom</a></li>
+	               <li><a href="#">Assortiment</a></li>
+	               <li><a href="#">Fotogalerij</a></li>
+	               <li><a href="#">Contact</a></li>
+	           </ul>
+	        </nav>
+	    </header>
+
